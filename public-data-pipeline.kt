055c44a21c8ab41f1b518de84d8d5717ed14789c
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2025-04-10 12:26:17 -0300

    Create public-data-pipeline.kt

---
+++ public-data-pipeline.kt
@@ -0,0 +1,16 @@
+public-data-pipeline/
+├── data/
+│   ├── raw/
+│   │   └── population_ibge.csv ← gerado pelo script
+│   └── processed/
+│
+├── scripts/
+│   ├── extract_ibge.py
+│   └── load_bigquery.py
+│
+├── notebooks/
+│   └── population_analysis.py
+│
+├── dashboards/
+├── requirements.txt
+└── README.md
